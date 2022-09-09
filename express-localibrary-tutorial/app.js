7bd709a0d61d8a1bed5c938a37fb199b8d8dd0c9
tcheng9 <tcheng789@gmail.com>
2022-09-08 23:12:51 -0500

    update - removing mongodb atlas error

--- express-localibrary-tutorial/app.js
+++ express-localibrary-tutorial/app.js
@@ -15,7 +15,7 @@ const wiki = require('./routes/wiki.js');
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://tcheng789:<mongo>@cluster0.23jlw1k.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://tcheng789:mongo1@cluster0.23jlw1k.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
